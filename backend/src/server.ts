716d508bc6aa04f4eebd9daab30080390d820722
Diego Aquino <diego.cruz001@gmail.com>
2020-10-13 16:54:57 -0300

    Add routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 
+import routes from './routes';
+
 const app = express();
 app.use(cors());
 // allow express to understand request.body as JSON
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
