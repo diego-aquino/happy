d5d6772a7aab3980ca288afbd614b5e1267b47cf
Diego Aquino <diego.cruz001@gmail.com>
2020-10-13 17:03:28 -0300

    Set up image upload

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import path from 'path';
 
 import './database/connection';
 
@@ -12,6 +13,7 @@ app.use(cors());
 // allow express to understand request.body as JSON
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
 app.listen(3333);
