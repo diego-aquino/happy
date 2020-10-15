530816f8dd44a0d947db7271f760e2f472168e6c
Diego Aquino <diego.cruz001@gmail.com>
2020-10-15 13:35:44 -0300

    Integrate OrphanagesMap with backend data

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
+/* eslint-disable arrow-parens */
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
-import mapMarkerImg from '../images/map-marker.svg';
-import '../styles/pages/orphanages-map.css';
+import api from '../services/api';
+import happyIcon from '../images/map-marker.svg';
+import mapMarker from '../resources/mapMarker';
+import '../styles/pages/OrphanagesMap.css';
+
+interface Orphanage {
+  id: number,
+  name: number,
+  latitude: number,
+  longitude: number
+}
 
 function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get('/orphanages').then(response => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
   return (
     <div className="pageOrphanagesMap">
       <aside>
         <header>
           <img
-            src={mapMarkerImg}
+            src={happyIcon}
             alt="Happy"
-            className="pageOrphanagesMap__mapMarker"
+            className="pageOrphanagesMap__happyIcon"
           />
 
           <h2 className="pageOrphanagesMap__asideTitle">
@@ -43,9 +60,33 @@ function OrphanagesMap() {
 
         {/* Using mapbox */}
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+        {orphanages.map(orphanage => (
+          <Marker
+            key={orphanage.id}
+            icon={mapMarker}
+            position={[orphanage.latitude, orphanage.longitude]}
+          >
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="pageOrphanagesMap__mapPopup"
+            >
+              {orphanage.name}
+
+              <Link
+                to={`/orphanages/${orphanage.id}`}
+                className="pageOrphanagesMap__accessOrphanage"
+              >
+                <FiArrowRight className="arrowRight" />
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
       </Map>
 
-      <Link to="/create" className="pageOrphanagesMap__createOrphanage">
+      <Link to="/orphanages/create" className="pageOrphanagesMap__createOrphanage">
         <FiPlus className="plusIcon" />
       </Link>
     </div>
