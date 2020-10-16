63db6223be588a957d66ede8ae44a06923435997
Diego Aquino <diego.cruz001@gmail.com>
2020-10-16 16:43:02 -0300

    Create styles provider

---
+++ mobile/src/styles/index.ts
@@ -0,0 +1,11 @@
+import OrphanagesMapStyles from './pages/OrphanageMapStyles';
+import OrphanageDetailsStyles from './pages/OrphanageDetailsStyles';
+import SelectMapPositionStyles from './pages/CreateOrphanage/SelectMapPositionStyles';
+import OrphanageDataStyles from './pages/CreateOrphanage/OrphanageDataStyles';
+
+export {
+  OrphanagesMapStyles,
+  OrphanageDetailsStyles,
+  SelectMapPositionStyles,
+  OrphanageDataStyles,
+};
