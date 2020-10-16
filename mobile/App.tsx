ca30adecfd8ba114acd30f9e4e32fd554a74aeab
Diego Aquino <diego.cruz001@gmail.com>
2020-10-16 16:12:07 -0300

    Add initial routes and fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useFonts } from 'expo-font';
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from '@expo-google-fonts/nunito';
+
+import Routes from './src/Routes';
 
 function App() {
-  return (
-    <View />
-  );
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  return (fontsLoaded ? <Routes /> : null);
 }
 
 export default App;
